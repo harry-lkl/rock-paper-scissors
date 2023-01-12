79f1a58bc0528f558a68a1a50024b2e50a8c0c08
harry-lkl <harryk742121@gmail.com>
2023-01-12 03:50:43 +0000

    complete working game

--- rps-game.js
+++ rps-game.js
@@ -5,17 +5,32 @@
     // store the selected string in the constant computerSelection
 
 const choice = ['rock', 'paper', 'scissors']
-const computerSelection = () => choice[Math.floor(Math.random() * choice.length)];
+const getComputerChoice = () => choice[Math.floor(Math.random() * choice.length)];
+const computerSelection = getComputerChoice();
 // console.log(typeof computerSelection);
 // console.log(computerSelection());
 
 
 // prompt player to select 'rock', 'paper', or 'scissors' and store it in a variable playerSelection
 // make it case insensitive, aka make input lower caps
-
-
+// pass choices into an array
 // compare the two selections with win conditions
-    // pass both choices into an array
-    // compare win conditions: ['rock','rock'] etc returns 'It's a draw!'
-
+    // compare win conditions: ['rock','rock'] etc returns 'It's a Draw!'
 // return a string that says something like "You Lose! Paper beats Rock"
+function playRound (string) {
+    const playerSelection = string.toLowerCase();
+    const getBothSelections = playerSelection + computerSelection;
+    console.log(getBothSelections);
+    if (getBothSelections === 'rockrock' || getBothSelections === 'paperpaper' || getBothSelections === 'scissorsscissors') {
+        return 'It\'s a draw!'
+    } else if (getBothSelections === 'paperrock' || getBothSelections === 'scissorspaper' || getBothSelections === 'rockscissors') {
+        return 'You Win!';
+    } else {
+        return 'You Lose!';
+    }
+}
+
+console.log(playRound('papeR'));
+
+
+// store result in a variable

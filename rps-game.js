b09aa2f09800744063082a3ad6fb62a849172f95
harry-lkl <harryk742121@gmail.com>
2023-01-12 01:59:40 +0000

    develop algorihm and write code to generate random choice for computer

---
+++ rps-game.js
@@ -0,0 +1,20 @@
+// return a random choice from 'rock', 'paper', or 'scissors' and
+// store it in a variable computerSelection, name funtion getComputerChoice
+    // put 'rock', 'paper', 'scissors' into an array called choices
+    // generate a random number from 0, 1, 2, and use it to select one of the selections from choice
+    // store the selected string in the constant computerSelection
+
+const choice = ['rock', 'paper', 'scissors']
+getComputerChoice = () => choice[Math.floor(Math.random() * choice.length)];
+const computerSelection = getComputerChoice();
+// console.log(typeof computerSelection);
+// console.log(computerSelection);
+
+
+// prompt player to select 'rock', 'paper', or 'scissors' and store it in a variable playerSelection
+// make it case insensitive
+
+
+// compare the two selections with win conditions 
+
+// return a string that says something like "You Lose! Paper beats Rock"
